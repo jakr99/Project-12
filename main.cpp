965c66d8df428f4da8467a22e2a3a4729a2ed49f
Jake Lee <jkln3w@umsystem.edu>
2023-10-16 22:48:09 -0500

    problem 2

--- main.cpp
+++ main.cpp
@@ -8,11 +8,16 @@ int main()
 {
     TrianglePrinter t;
 
-    for(unsigned int i = 0; i < 10; i++)
+    for(unsigned int i = 0; i < 7; i++)
     {
+        t.newrow();
+        for (unsigned int k = 0; k  <= i; k++){
+            t.add(combination(i, k));
+        }
         cout << factorial(i) << endl;
     }
 
+    t.print();
     cout << " n=5 m=2 " << combination(5,2) << endl;
 
     return 0;
